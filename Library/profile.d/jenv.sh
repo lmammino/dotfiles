dc202513e480d8a7a205b545f41cb9d3c6579deb
Luciano Mammino <lucianomammino@gmail.com>
2020-04-09 09:44:02 +0100

    updated jenv setup

--- Library/profile.d/jenv.sh
+++ Library/profile.d/jenv.sh
@@ -1,7 +1,6 @@
 # jenv (https://www.jenv.be/)
 if which jenv >/dev/null 2>&1 ; then
-  if [ -d "$HOME/.jenv/bin" ]; then
-    export PATH="$HOME/.jenv/bin:$PATH"
+  if [ -d "$HOME/.jenv" ]; then
     eval "$(jenv init -)"
   fi
 fi
