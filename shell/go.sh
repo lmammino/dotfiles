063fd293af57ca59e9b7d3865de394c2ff7b7b08
Luciano Mammino <lucianomammino@gmail.com>
2021-06-20 20:43:07 +0100

    Updated go module to work by default with brew default settings

--- shell/go.sh
+++ shell/go.sh
@@ -1,8 +1,8 @@
 # Support for go
 
-if test -d "/usr/local/go" && test -d "$HOME/local/go"
-  set -x GOROOT "/usr/local/go";
-  set -x GOPATH "$HOME/local/go";
-  set -x PATH $PATH $GOPATH/bin $GOROOT/bin;
+if test -d "/usr/local/opt/go/libexec" && test -d "$HOME/go"
+  set -x GOROOT "/usr/local/opt/go/libexec";
+  set -x GOPATH "$HOME/go";
+  set -x PATH $GOPATH/bin $GOROOT/bin $PATH;
   set -x GO111MODULE on;
 end
