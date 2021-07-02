0555396633ec56c64c699e08c4a13454bd1889ce
Luciano Mammino <luciano.mammino@fourtheorem.com>
2021-07-02 05:52:19 +0100

    Extended brew config to support default bin path

--- shell/brew.sh
+++ shell/brew.sh
@@ -4,3 +4,10 @@
 if test -d "/usr/local/sbin"
   set -x PATH /usr/local/sbin $PATH
 end
+
+# exposes /opt/homebrew/bin where brew 
+# installs some binaries (e.g. python)
+if test -d "/opt/homebrew/bin"
+  set -x PATH /opt/homebrew/bin $PATH
+end
+
