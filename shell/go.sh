3f0b00238f23486f8c8ee3b69483ea756b206ff7
Luciano Mammino <luciano.mammino@fourtheorem.com>
2021-10-31 18:30:30 +0000

    Updated go def

--- shell/go.sh
+++ shell/go.sh
@@ -1,8 +1,6 @@
 # Support for go
 
-if test -d "/usr/local/opt/go/libexec" && test -d "$HOME/go"
-  set -x GOROOT "/usr/local/opt/go/libexec";
-  set -x GOPATH "$HOME/go";
-  set -x PATH $GOPATH/bin $GOROOT/bin $PATH;
+if test -d "$HOME/go"
+  set -x PATH $HOME/go/bin $PATH;
   set -x GO111MODULE on;
 end
