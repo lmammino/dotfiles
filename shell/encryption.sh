93034c85e45c89708638fed1e034016ef72a5b0f
Luciano Mammino <lucianomammino@gmail.com>
2022-11-13 20:26:15 +0000

    updated enc helpers

--- shell/encryption.sh
+++ shell/encryption.sh
@@ -8,11 +8,11 @@
 #  dec SOMEFILE.enc -out SOMEFILE.txt
 
 if which openssl >/dev/null 2>&1
-  function dec --description 'decrypts data from a file using openssl aes128 base64. Usage: enc SOMEFILE.txt -out SOMEFILE.enc'
+  function dec --description 'decrypts data from a file using openssl aes128 base64. Usage: dec SOMEFILE.txt'
     openssl enc -d -aes128 -base64 -in $argv
   end
 
-  function enc --description 'encrypts data from a file using openssl aes128 base64. Usage: dec SOMEFILE.enc -out SOMEFILE.txt'
+  function enc --description 'encrypts data from a file using openssl aes128 base64. Usage: enc SOMEFILE.enc -out SOMEFILE.txt'
     openssl enc -e -aes128 -base64 -in $argv
   end
 end
