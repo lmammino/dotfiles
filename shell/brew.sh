8bd5c601cfb3dd7004173576aeed300cd07f5cd6
Luciano Mammino <lucianomammino@gmail.com>
2023-07-02 13:34:59 +0100

    updated brew to expose correct sbin location

--- shell/brew.sh
+++ shell/brew.sh
@@ -1,13 +1,15 @@
-# exposes /usr/local/sbin where brew 
-# installs some binaries (e.g. python)
-
+# exposes /usr/local/sbin
 if test -d "/usr/local/sbin"
   set -x PATH /usr/local/sbin $PATH
 end
 
-# exposes /opt/homebrew/bin where brew 
-# installs some binaries (e.g. python)
+# exposes /opt/homebrew/bin
 if test -d "/opt/homebrew/bin"
   set -x PATH /opt/homebrew/bin $PATH
+end
+
+# exposes /opt/homebrew/sbin
+if test -d "/opt/homebrew/sbin"
+  set -x PATH /opt/homebrew/sbin $PATH
 end
 
